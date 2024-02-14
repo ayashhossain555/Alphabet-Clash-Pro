f5e344f06097fd3354f94a96229e1789af40a4a1
ayashhossain555 <ayash.ahmedh555@gmail.com>
2024-02-15 03:52:20 +0600

    updated the game

--- scripts/alpha_clash.js
+++ scripts/alpha_clash.js
@@ -1,14 +1,57 @@
 console.log(`Alpha Clash Running`);
 
+document.addEventListener('keyup',function handleKeyPress(event){
+    const pressed = event.key;
+
+    if(pressed === 'Escaped'){
+        gameOver();
+    }
+
+    const currentAlphabet = document.getElementById('current-alphabet').innerText;
+    if(pressed === currentAlphabet){
+        console.log(`You get a point :)`);
+        let currentScore = getElementValue('current-score');
+        currentScore++;
+        setElementValue('current-score',currentScore);
+
+        removeBanckgroundColorById(currentAlphabet);
+        continueGame();
+    }
+    else{
+        console.log(`You lost a life :(`);
+        let currentLife = getElementValue('current-life');
+        currentLife--;
+        setElementValue('current-life',currentLife);
+
+        if(currentLife === 0){
+            console.log(`Game Over -_- `);
+            gameOver();
+        }
+
+        removeBanckgroundColorById(currentAlphabet);
+        continueGame();
+    }
+})
+
 function continueGame(){
     const alphabet = getARandomAlphabet();
-    const currentAlphabet = document.getElementById('current-alphabet')
+    const currentAlphabet = document.getElementById('current-alphabet');
     currentAlphabet.innerText = alphabet;
     setBanckgroundColorById(alphabet);
 }
 
 function play(){
-    changeInterface('home','play');
+    changeInterface('home','score','play');
+    setElementValue('current-life',5);
+    setElementValue('current-score',0);
     continueGame();
 }
 
+function gameOver(){
+    changeInterface('play','home','score');
+    const lastScore = getElementValue('current-score');
+    setElementValue('last-score',lastScore);
+
+    const currentAlphabet = getElementText('current-alphabet');
+    removeBanckgroundColorById(currentAlphabet);
+}
--- scripts/utility.js
+++ scripts/utility.js
@@ -1,6 +1,9 @@
-function changeInterface(hideElementId, showElementId){
+function changeInterface(hideElementId, hideElementId2, showElementId){
     const hideElement = document.getElementById(hideElementId);
     hideElement.classList.add('hidden');
+
+    const hideElement2 = document.getElementById(hideElementId2);
+    hideElement2.classList.add('hidden');
 
     const showElement = document.getElementById(showElementId);
     showElement.classList.remove('hidden');
@@ -13,7 +16,26 @@ function getARandomAlphabet(){
     return alphabets[index];
 }
 
+function getElementText(elementId){
+    const currentElement = document.getElementById(elementId);
+    return currentElement.innerText;
+}
+
+function getElementValue(elementId){
+    const currentElement = document.getElementById(elementId);
+    return parseInt(currentElement.innerText);
+}
+
+function setElementValue(elementId, elementValue){
+    const currentElement = document.getElementById(elementId);
+    currentElement.innerText = elementValue;
+}
+
 function setBanckgroundColorById(elementId){
     const element = document.getElementById(elementId);
     element.classList.add('bg-orange-400')
+}
+function removeBanckgroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-orange-400')
 }
